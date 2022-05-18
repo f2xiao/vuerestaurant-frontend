64e738e6aecf35019b4da2c899ed4af09ee085eb
f2xiao <fxiao.mecheng@gmail.com>
2022-05-18 16:03:24 -0400

    Add homeview

---
+++ src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container class='home' style="height:85vh">
+    <v-row  align="center" class="fill-height">
+       <v-col align-self="end" cols="sm-6" order-sm='2'>
+          <h2 class="text-h4 sh font-italic">the most authenticated Southern Chinese Food</h2>
+      </v-col>
+      <v-col align-self="start" cols="sm-6">
+          <h1  class="text-h1 ph">Oriental Pearl</h1>
+      </v-col>
+    </v-row>
+        
+  </v-container>
+</template>
+
+<script>
+export default {
+    name:'Home'
+}
+</script>
+
+<style lang="scss">
+.home {
+  height: 100%;
+  background-image: url(@/assets/img/hero.png);
+  background-size: auto;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+  @keyframes moveInLeft {
+  0% {
+    opacity: 0;
+    transform: translateX(-10rem);
+  }
+  80% {
+    transform: translateX(1rem);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+@keyframes moveInRight {
+  0% {
+    opacity: 0;
+    transform: translateX(10rem);
+  }
+  80% {
+    transform: translateX(-1rem);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+.ph{
+  animation: moveInRight 1.5s ease-in-out
+}
+.sh{
+  animation: moveInLeft 1.5s ease-in-out
+}
+}
+
+</style>
---
+++ src/components/layout/HeaderBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar shaped flat color="primary">
+      <v-app-bar-title>
+        <v-img src="/logo.png" height="48" width="64"></v-img>
+      </v-app-bar-title>
+     <v-btn to="home">
+      home
+     </v-btn>
+     <v-btn to="menu">
+      menu
+     </v-btn>
+     |
+      <v-btn icon to="api/search">
+        <v-icon>mdi-magnify</v-icon>
+     </v-btn>
+     <v-btn icon to="/account">
+         <v-icon>mdi-account</v-icon>
+     </v-btn>
+     <v-btn icon to="/cart">
+        <v-icon>mdi-cart</v-icon>
+        <v-spacer></v-spacer>
+        <span>0</span>
+     </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    name: 'header-bar',
+  //   computed: {
+  //   ...mapGetters({
+  //     cartTotalQuantity:'cart/cartTotalQuantity'
+  //   })
+  // }
+}
+</script>
