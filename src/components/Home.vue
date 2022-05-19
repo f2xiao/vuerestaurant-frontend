315321be51b9b094f37fe37c870989dbbf9a9701
f2xiao <fxiao.mecheng@gmail.com>
2022-05-19 10:27:59 -0400

    Add a button on homeview

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,10 +2,25 @@
   <v-container class='home' style="height:85vh">
     <v-row  align="center" class="fill-height">
       <v-col align-self="end" cols="sm-6">
-           <h2 class="text-md-h4 sh font-italic">the most authenticated Southern Chinese Food</h2>
+        <v-row style="height:15vh">
+          <v-col offset-md="6">
+            <v-btn color=" primary" to="/menu">
+              Order Now
+            </v-btn>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <transition name="moveinleft" appear>
+            <h2 class="text-md-h4 font-italic">the most authenticated Southern Chinese Food</h2>
+          </transition>
+          </v-col>
+        </v-row>
       </v-col>
-       <v-col align-self="start" cols="sm-6" order-sm='2'> 
-          <h1  class="text-md-h1 ph">Oriental Pearl</h1>
+       <v-col align-self="start" cols="sm-6" order-sm='2'>
+        <transition name="moveinright" appear>
+          <h1  class="text-md-h1">Oriental Pearl</h1>
+        </transition>
       </v-col>
     </v-row>
         
@@ -26,38 +41,37 @@ export default {
   background-repeat: no-repeat;
   background-position: center;
   position: relative;
-  @keyframes moveInLeft {
-  0% {
-    opacity: 0;
-    transform: translateX(-10rem);
+  @keyframes moveinLeft {
+    0% {
+      opacity: 0;
+      transform: translateX(-10rem);
+    }
+    80% {
+      transform: translateX(1rem);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
   }
-  80% {
-    transform: translateX(1rem);
+  @keyframes moveinRight {
+    0% {
+      opacity: 0;
+      transform: translateX(10rem);
+    }
+    80% {
+      transform: translateX(-1rem);
+    }
+    100% {
+      opacity: 1;
+      transform: translateX(0);
+    }
   }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
+  .moveinright-enter-active{
+    animation: moveinRight 1.5s ease-in-out
+  }
+  .moveinleft-enter-active{
+    animation: moveinLeft 1.5s ease-in-out
   }
 }
-@keyframes moveInRight {
-  0% {
-    opacity: 0;
-    transform: translateX(10rem);
-  }
-  80% {
-    transform: translateX(-1rem);
-  }
-  100% {
-    opacity: 1;
-    transform: translateX(0);
-  }
-}
-.ph{
-  animation: moveInRight 1.5s ease-in-out
-}
-.sh{
-  animation: moveInLeft 1.5s ease-in-out
-}
-}
-
 </style>
